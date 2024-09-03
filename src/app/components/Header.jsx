695740fdf6901e96e7535c4dc5d8d6ed26051532
Praveen Gorakala <praveenasvenom@gmail.com>
2024-09-03 11:41:38 +0530

    responsive.

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,7 +6,8 @@ const Header = () => {
         into <p>Celebrations</p></h1>
         {/* <p>At Sehari Events, we craft intimate, memorable experiences with personalized attention, ensuring your cozy birthday, social gathering, themed event, or small-scale party is exactly as you envisioned—perfect in every way.</p> */}
       </div>
-        <img src="./headerLower.png"/>
+        <img className="deskOnly" src="./headerLower.png"/>
+        <img className="mobOnly" src="./headerLower.svg"/>
     </div>
   );
 };
